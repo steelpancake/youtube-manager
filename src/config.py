8c97ec646a9d934a7dbca09194c8ac96132ea49c
SteelPancake <makingpancakesmakingbakinpancakes@proton.me>
2023-12-22 14:35:19 +0300

    as before
    added github's python gitignore
    
    new ignore
    
    added list creation utils
    
    Delete config.json
    
    weird git bug
    
    added license
    
    download ready and ignore ability added too
    
    readme
    
    choose different formats for videos
    
    skipping prompt, format selection
    
    stupid readme fix
    
    arghhh readme

---
+++ src/config.py
@@ -0,0 +1,163 @@
+import json
+import os
+import sys
+import jsonpickle
+import main
+import copy
+
+
+class Config:
+    def __init__(self):
+        self.channels = []
+        self.opts = []
+        self.playlists = []
+        self.formats = {
+                "240": {
+                     'format': '(bv+(250/251/ba))',
+                     'format_sort': ['vcodec:vp9', 'acodec', 'res:240'],
+                     'prefer_free_formats': True,
+                },
+                "default": {
+                     'format': '(bv+(250/251/ba))[filesize<?50M][filesize_approx<?100M]',
+                     'format_sort': ['vcodec:vp9', 'acodec', 'res:480'],
+                     'prefer_free_formats': True,
+                },
+                "720": {
+                     'format': '(bv+(250/251/ba))',
+                     'format_sort': ['vcodec:vp9', 'acodec', 'res:720'],
+                     'prefer_free_formats': True,
+                },
+                "1080": {
+                     'format': '(bv+(250/251/ba))',
+                     'format_sort': ['vcodec:vp9', 'acodec', 'res:1080'],
+                     'prefer_free_formats': True,
+                }
+        }
+
+    def list_channels(self):
+        for i in range(len(self.channels)):
+            print(str(i) + "\t" + str(self.channels[i]))
+
+    def list_playlists(self):
+        for playlist in self.playlists:
+            print(playlist)
+
+    def reset_formats(self):
+        new_conf = Config()
+        self.formats = new_conf.formats
+
+
+class Channel:
+    import main
+    def __init__(self):
+        self.url = ""
+        self.nick = ""
+        self.dir = ""
+        self.ignore = False
+        self.deleted = False
+        self.ignored_videos = []
+        self.info_dict = {}
+        self.preferred_format = "default"
+
+    def __repr__(self):
+        return "<Channel nick: '% s', \turl: % s>" % (self.nick, self.url)
+
+    def from_dict(self, dictionary):
+        self.url = dictionary["url"]
+        self.nick = dictionary["nick"]
+        self.dir = dictionary["dir"]
+        self.ignore = dictionary["ignore"]
+        self.deleted = dictionary["deleted"]
+        self.ignored_videos = dictionary["ignored_videos"]
+        self.info_dict = dictionary["info_dict"]
+        return self
+
+    def from_info_dict(self, info):
+        self.url = Channel.url_from_id(info["channel_id"])
+        self.nick = input("any nickname for this channel? (leave blank for no) ") or info["uploader"]
+        self.dir = self.nick
+        self.info_dict = info
+        self.ignore = False
+        self.deleted = False
+        self.ignored_videos = {}
+        return self
+
+    def url_from_id(url: str):
+        return ("https://www.youtube.com/channel/" + url)
+
+    def playlist_url_from_id(id: str):
+        string = id[2:]
+        return "https://www.youtube.com/playlist?list=" + "UU" + string
+
+    def is_url_in_list(url: str, arr):
+        for entry in arr:
+            if entry.url == url:
+                return True
+        return False
+
+    def find_in_list(url: str, arr=None):
+        arr = [arr]
+        if not arr[0]:
+            import main
+            arr[0] = main.globs.conf.channels
+        arr = arr[0]
+        for i in range(len(arr)):
+            if arr[i].url == url:
+                return i
+
+    def opts_with_preferred_format(self, opts: dict):
+        import main
+        opts = copy.copy(opts)
+        format = main.this.globs.conf.formats[self.preferred_format]
+        opts.update(format)
+        return opts
+
+
+class Video:
+    def __init__(self):
+        pass
+
+    def is_video(info):
+        try:
+            return info["webpage_url_basename"] == "watch"
+        except:
+            return False
+
+
+def check_make_archive(path="archive.txt"):
+    if not os.path.exists(path):
+        try:
+            f = open(path, "x")
+            f.close()
+        except:
+            print(format("could not create archive file:  {}", path), file=sys.stderr)
+            sys.exit(1)
+
+
+def get(path = "config.json"):
+    if not os.path.exists(path):
+        try:
+            f = open(path, "x")
+            f.close()
+            with open(path, "r+") as f:
+                f.write(jsonpickle.encode(Config(), indent=4))
+            return Config()
+        except:
+            print(format("could not create configuration file:  {}", path), file=sys.stderr)
+            sys.exit(1)
+
+    conf = None
+    with open(path, "r") as f:
+        conf = jsonpickle.decode(f.read())
+    print(conf)
+    return conf
+
+
+def save(conf, path = "config.json"):
+    with open(path, "w") as f:
+        f.write(jsonpickle.encode(conf, indent=4))
+
+
+def add_channel(conf, channel):
+    if not Channel.is_url_in_list(channel.url, conf.channels):
+        conf.channels.append(channel)
---
+++ src/skip_playlist.py
@@ -0,0 +1,61 @@
+#!/bin/python3
+import yt_dlp
+import sys
+import time
+import multiprocessing
+
+"""
+This Script also works on normal playlists
+usage: ./<script> <playlist url> <start> <end>
+
+How it works:
+     It prints an output that can be pasted into yt-dlp archive.txt
+"""
+
+args = sys.argv
+playlist = args[1]
+album_name:str = ''
+
+# number of threads that will spawn 
+# change this value accordingly
+max_processes=8
+
+ydl_opts_playlist_extractor = {}
+ydl_opts = {}
+
+range_start = int(args[2])
+range_end = int(args[3])
+
+def main():
+    ydl_opts_playlist_extractor = {
+        'extract_flat': True,
+        'skip_download': True,
+    }
+    ydl_extractor = yt_dlp.YoutubeDL(ydl_opts_playlist_extractor)
+
+    #exits incase an error occurs
+    try:
+        info_dict = ydl_extractor.extract_info(playlist, download=False, process=False)
+    except:
+        exit()
+
+    # remove 'Album - ' from playlist name
+    global album_name
+    album_name = info_dict['title'].replace('Album - ', '')
+
+    playlist_entries = []
+    for videos in info_dict['entries']:
+        playlist_entries.append(videos)
+
+    playlist_digits = len(str(len(playlist_entries)))
+
+    # get the urls to perform download
+    playlist_urls = []
+    index = 1
+    for video in playlist_entries:
+        if index >= range_start and index <= range_end:
+            print("youtube "+video["id"])
+        index += 1
+
+if __name__ == "__main__":
+    main()
